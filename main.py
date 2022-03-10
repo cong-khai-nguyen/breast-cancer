e247ee9c2071a30d1be3c42f281ccd4fa9fc0a36
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-10 16:18:07 -0600

    compare the accuracy between KNN model and SVM model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 from sklearn import svm
 from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
+
 cancer = datasets.load_breast_cancer()
 
 # print(cancer.feature_names)
@@ -13,5 +14,30 @@ y = cancer.target
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.2)
 
-print(y_train)
+
+# Use SVC without adding any parameters
+# clf = svm.SVC()
+# clf.fit(x_train, y_train)
+# y_pred = clf.predict(x_test)
+# accuracy = metrics.accuracy_score(y_test, y_pred)
+# # 87.719298%
+# print(format(accuracy, "%"))
+
+# use one kernel: linear with soft/hard margin or poly with degree
+clf = svm.SVC(kernel = "linear", C=2)
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, y_pred)
+# 98.245614%
+print(format(accuracy, "%"))
+
+# Use KNN model to classify
+clf = KNeighborsClassifier(n_neighbors=9)
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, y_pred)
+# 92.105263%
+print(format(accuracy, "%"))
+
+
 classes = ['malignant', 'benign']
